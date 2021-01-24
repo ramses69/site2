29b085d3a4bff107df30ac39a021e8a6089c1b0d
ramses69 <71641041+ramses69@users.noreply.github.com>
2021-01-24 19:08:37 +0200

    Add files via upload

---
+++ project/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js"></script>
+    
+    <script src="..//project/js/script.js" ></script>
+    <link rel="stylesheet" href="../project/css/main.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet">
+ <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Главная</title>
+</head>
+<body link="white" vlink="white" >
+    <div id="snow-animation-container"></div>
+    
+    <ul class="nav">
+        <li><a href="index.html">Главная</a></li>
+        <li><a href="index2.html">Об ПО</a></li>
+        <li><a href="index3.html">Обучение</a></li>
+        <li><a href="index4.html">О сайте</a></li>
+        
+    </ul>
+    <h1 class="cc">Welcome to 3D</h1>
+    <div class="ptext">
+        <p>В этой статье мы рассмотрим несколько самых популярных программ на нынешний момент</p>
+        <p>Этот сайт поможет Вам определиться с напрямлением в 3D и даст базовые знание об этой сфере.</p>
+
+    </div >
+    
+    <ul class="ulls">
+        <li> <a href="https://www.blender.org/">Blender 3D</a></li>
+        <li> <a href="https://www.maxon.net/en/cinema-4d">Cinema 4D</a></li>
+        <li> <a href="https://pixologic.com/">ZBrush</a></li>
+    </ul>
+ <div class="pvideo" >
+     <p>Рекомендую к просмотру</p>
+     <iframe   width="560" height="315"  src="https://www.youtube.com/embed/WhWc3b3KhnY" frameborder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+ </div>
+</body>
+</html>
